910cceb0ac1b843ea0692c9f5e3c6c533b26c5ad
AndrewJWin <fastroute66@live.com>
2021-04-16 11:33:18 -0500

    Update questions

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-<div id="app">
-  <h1>Would you rather...</h1>
-  <would-you-rather 
-    v-bind:question="wyrQuestion"
-    v-bind:answer1="wyrAnswer1"
-    v-bind:answer2="wyrAnswer2"
-    v-on:answer-changed="answerChanged">
-  </would-you-rather>
+  <div id="app">
+    <h1>Would you rather...</h1>
+    <would-you-rather v-for="question in wyrQuestions"
+      v-bind:key="question.id"
+      v-bind:question=question
+      v-on:answer-changed="answerChanged">
+    </would-you-rather>
 
-  <p>{{userSelectionMessage}}</p>
-
-</div>
+    <div id="answers">
+      <h1>You would rather...</h1>
+      <ul v-if="userSelection.length > 0" >
+        <li v-for="choice in choicesAvailable" v-bind:key="choice">{{choice}}</li>
+      </ul>
+      <p v-else>Not answer it seems? Please make a selection above.</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,21 +27,44 @@ export default {
   },
   data() {
     return {
-      wyrQuestion: "Would you rather be able to speak any language but not be able to read in any of them or read any language but not be able to speak any of them?",
-      wyrAnswer1: "Speak any language",
-      wyrAnswer2: "Read every language",
-      userSelectionMessage: ""
+      wyrQuestions: [
+        {
+          id: 0,
+          wyrQuestion: "Would you rather be able to speak any language but not be able to read in any of them or read any language but not be able to speak any of them?",
+          wyrAnswer1: "Speak any language",
+          wyrAnswer2: "Read every language",
+        },
+        {
+          id: 1,
+          wyrQuestion: "Would you rather be able to find anything that was lost or every time you touched someone they would be unable to lie?",
+          wyrAnswer1: "Find everything",
+          wyrAnswer2: "Touch for truth",
+        },
+        {
+          id: 2,
+          wyrQuestion: "Would you rather be able to remember everything in every book you read or remember every conversation you have?",
+          wyrAnswer1: "Remember the book",
+          wyrAnswer2: "Remember the speech",
+        }
+      ],
+      userSelection: []
     }
   },
   methods: {
-    answerChanged(choice) {
-      this.userSelectionMessage = `Thanks, you chose to ${choice}`
+    answerChanged(id, choice) {
+      this.userSelection[id] = choice;
+    }
+  },
+  computed: {
+    choicesAvailable() {
+      // Avoid displaying not-yet-chosen choices
+      return this.userSelection.filter( choice => choice )
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -46,6 +73,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#answers {
+  font-family: Roboto, sans-serif;
+  font-size: 15pt;
+}
+
+li {
+  text-align: inherit;
+  list-style-type: disc;
+  list-style-position: inside;
+  font-size: 1.2em;
 }
 
 p {
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="wyr">
-    <h2>Please make your choice!</h2>
-
-    <h3>{{question}}</h3>
-
-    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade"><label>{{answer1}}</label>
-    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade"><label>{{answer2}}</label>
+    <h2>Would you rather?</h2>
+    <h3>{{question.wyrQuestion}}</h3>
+    
+    <input v-bind:id="question1" type="radio" 
+      v-bind:value="question.wyrAnswer1"
+      v-model="choice"
+      v-on:change="choiceMade">
+    <label for="question1">{{ question.wyrAnswer1 }}</label>
+    
+    <input v-bind:id="question2" type="radio"
+      v-bind:value="question.wyrAnswer2"
+      v-model="choice"
+      v-on:change="choiceMade">
+    <label for="question2">{{ question.wyrAnswer2 }}</label>
   </div>
 </template>
 
@@ -13,27 +21,24 @@
 export default {
   name: 'WouldYouRather',
   props: {
-    question: String,
-    answer1: String,
-    answer2: String
+    question: Object
   },
   data() {
     return {
-      choice:""
+      choice:String
     }
   },
   methods: {
     choiceMade() {
-      this.$emit("answer-changed", this.choice)
+      this.$emit("answer-changed", this.question.id, this.choice)
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin: 20px 0 0;
 }
 
 .wyr {
